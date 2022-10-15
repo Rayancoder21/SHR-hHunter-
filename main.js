f83c55328d90aa9ba5c993d3e934e8caab4f9be3
Rayancoder21 <100302458+Rayancoder21@users.noreply.github.com>
2022-10-15 13:47:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const form = document.getElementById("form");
+form.addEventListener('submit', logSubmit);
+
+function logSubmit(event) {
+    event.preventDefault();
+
+    const degree = document.getElementById("degree");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+    const Experience = document.getElementById("Experience");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+    const Languages = document.getElementById("Languages");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+}
+
+const assistant = document.getElementById("assistant");
+form.addEventListener('submit', logSubmit);
+
+function logSubmit(event) {
+    event.preventDefault();
+
+    const comunicationskills = document.getElementById("comunication skills");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+    const Experience = document.getElementById("Experience");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+    const Languages = document.getElementById("Languages");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+}
+
+const designer = document.getElementById("designer");
+form.addEventListener('submit', logSubmit);
+
+function logSubmit(event) {
+    event.preventDefault();
+
+    const comunicationskills = document.getElementById("comunication skills");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+    const Experience = document.getElementById("Experience");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+    const degree = document.getElementById("degree");
+    var option = degree.options[degree.selectedIndex];
+    console.log(option.value)
+}
